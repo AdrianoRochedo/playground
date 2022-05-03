c8da8d294984facfcbe7f0b8e81dccb12a341d1e
rochedo <rochedo@gmail.com>
2022-05-03 13:16:45 -0300

    C++ => Math and Random numbers

--- C/Console/Console.cpp
+++ C/Console/Console.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 void TypeSizes();
 void TypeDef();
 void EnumTypes();
 void ConstKeyword();
 void Files();
+void Math();
+void Random();
 
 int main()
 {
@@ -16,6 +19,8 @@ int main()
     EnumTypes();
     ConstKeyword();
     Files();
+    Math();
+    Random();
 
     auto key = std::getchar();
 
@@ -121,4 +126,41 @@ void Files()
 
     // close the opened file.
     infile.close();
+}
+
+void Math()
+{
+    title("Math");
+
+    // number definition:
+    short  s = 10;
+    int    i = -1000;
+    long   l = 100000L;
+    float  f = 230.47F;
+    double d = 200.374;
+
+    // mathematical operations;
+    std::cout << "sin(d) :"     << sin(d)    << std::endl;
+    std::cout << "abs(i)  :"    << abs(i)    << std::endl;
+    std::cout << "floor(d) :"   << floor(d)  << std::endl;
+    std::cout << "sqrt(f) :"    << sqrt(f)   << std::endl;
+    std::cout << "pow( d, 2) :" << pow(d, 2) << std::endl;
+}
+
+void Random()
+{
+    title("Random");
+
+    int i, j;
+
+    // set the seed
+    srand((unsigned)time(NULL));
+
+    /* generate 10  random numbers. */
+    for (i = 0; i < 10; i++) 
+    {
+        // generate actual random number
+        j = rand();
+        std::cout << " Random Number : " << j << std::endl;
+    }
 }
