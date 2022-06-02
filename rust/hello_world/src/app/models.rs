ef5cd16292bf4ae47814f9e22b722db2bd96d2a9
rochedo <rochedo@gmail.com>
2022-06-02 20:23:53 -0300

    rust => heranca de traits

--- rust/hello_world/src/app/models.rs
+++ rust/hello_world/src/app/models.rs
@@ -1,5 +1,5 @@
 
-// Rust nao tem heranca pois nao eh OOP full
+// Rust nao tem heranca (da implementacao) pois nao eh OOP full .. mas (olhar mais abaixo)
 
 // -----------------------------------------------
 
@@ -60,3 +60,41 @@ impl Animal for Gato
         println!("miau");
     }    
 }
+
+// ----------------------------------------------
+
+// mas a herenca de traits eh permitida
+
+// https://doc.rust-lang.org/rust-by-example/trait/supertraits.html
+
+trait Person {
+    fn name(&self) -> String;
+}
+
+// Person is a supertrait of Student.
+// Implementing Student requires you to also impl Person.
+trait Student: Person {
+    fn university(&self) -> String;
+}
+
+trait Programmer {
+    fn fav_language(&self) -> String;
+}
+
+// CompSciStudent (computer science student) is a subtrait of both Programmer 
+// and Student. Implementing CompSciStudent requires you to impl both supertraits.
+trait CompSciStudent: Programmer + Student {
+    fn git_username(&self) -> String;
+}
+
+fn comp_sci_student_greeting(student: &dyn CompSciStudent) -> String {
+    format!(
+        "My name is {} and I attend {}. My favorite language is {}. My Git username is {}",
+        student.name(),
+        student.university(),
+        student.fav_language(),
+        student.git_username()
+    )
+}
+
+// .. implementar todos os traits num tipo concreto
