e5323922cb64f491e6698c301576a3aafdded54d
rochedo <rochedo@gmail.com>
2022-06-03 17:03:45 -0300

    node => classes e TS

--- node/SimpleNodeConsoleApp/src/app.js
+++ node/SimpleNodeConsoleApp/src/app.js
@@ -1,4 +1,5 @@
 var utils = require("./utils");
+var classes = require("./class");
 
 console.clear();
 
@@ -11,12 +12,20 @@ console.log();
 utils.lerArquivo();
 console.log();
 
-var pessoa = utils.retornarUmaPessoa();
+const pessoa = utils.retornarUmaPessoa();
 console.log(pessoa);
 console.log();
 
-var pessoas = utils.retornarPessoas();
+const pessoas = utils.retornarPessoas();
 console.log(pessoas);
+console.log();
+
+const rec = new classes.Rectangle(5, 20);
+console.log(rec.toString());
+console.log();
+
+const square = new classes.Square(5);
+console.log(square.toString());
 console.log();
 
 utils.keyPress();
